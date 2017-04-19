38d919215798882a019bd35624c109d5cbab9ade
robot9706 <robot9706@gmail.com>
2017-04-19 21:11:48 +0200

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 5a7a60b1957ff5054a9965ff77591579cb4ec5b2.

--- TG_TC/FileCommander/src/com/javamegvan/tc/ui/filetable/FileBrowserEventListener.java
+++
@@ -1,7 +0,0 @@
-package com.javamegvan.tc.ui.filetable;
-
-public interface FileBrowserEventListener {
-	void onPathChange(FileBrowserTable source);
-	void onSelectionChange(FileBrowserTable source);
-	void onSwitchSide();
-}
