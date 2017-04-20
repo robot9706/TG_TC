bd5ef31529133c7101af7eccf31e4cb9250d266b
Skezzowski <sipblai@gmail.com>
2017-04-21 00:06:47 +0200

    Kezdetleges csík

--- FileCommander/src/com/javamegvan/tc/ui/funcbtn/FunctionCopy.java
+++ FileCommander/src/com/javamegvan/tc/ui/funcbtn/FunctionCopy.java
@@ -8,6 +8,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import javax.swing.JFrame;
+import javax.swing.ProgressMonitor;
+import javax.swing.SwingWorker;
+
 import com.javamegvan.tc.ui.MainFrame;
 
 public class FunctionCopy implements FunctionButton {
@@ -27,17 +31,32 @@ public class FunctionCopy implements FunctionButton {
 	public String getName() {
 		return "Másolás";
 	}
+	
 
+	ProgressMonitor pB = new ProgressMonitor(null,"Copy..","",0,100);
+	
 	public void doFunction(MainFrame frame) {
-		// TODO: CSÍK
-
+		// TODO: CSÍK tökéletesítés
+		
+		pB.setProgress(0);
+		
 		File source = new File(frame.getFocusedFile().getPath());
 		File dest = new File(frame.getunFocusedBrowser().getCurrentFolder().getPath(),
 				frame.getFocusedFile().getName());
 
 		try {
 			if (frame.getFocusedBrowser().getSelectedFiles(false).isEmpty()) {
-				copy(source, dest);
+				@SuppressWarnings("rawtypes")
+				SwingWorker worker = new SwingWorker(){
+
+					@Override
+					protected Object doInBackground() throws Exception {
+						copy(source, dest);
+						frame.getunFocusedBrowser().navigateTo(frame.getunFocusedBrowser().getCurrentFolder());
+						return null;
+					}
+				};
+				worker.execute();
 			} else {
 				for (File f : frame.getFocusedBrowser().getSelectedFiles(false)) {
 					File d = new File(frame.getunFocusedBrowser().getCurrentFolder().getPath(), f.getName());
@@ -48,8 +67,6 @@ public class FunctionCopy implements FunctionButton {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-		frame.getunFocusedBrowser().navigateTo(frame.getunFocusedBrowser().getCurrentFolder());
 
 	}
 
@@ -68,15 +85,24 @@ public class FunctionCopy implements FunctionButton {
 				copy(srcFile, destFile);
 			}
 		} else {
-
+			
 			InputStream in = new FileInputStream(src);
 			OutputStream out = new FileOutputStream(dest);
 
+			long expectedBytes = src.length();
 			byte[] buffer = new byte[1024];
 
+			
+			long totalBytesCopied=0;
 			int length;
+			int progress=0;
 			while ((length = in.read(buffer)) > 0) {
 				out.write(buffer, 0, length);
+				
+				totalBytesCopied+= length;
+			    progress = (int)Math.round(((double)totalBytesCopied / (double)expectedBytes)*100);
+				pB.setProgress(progress);
+				pB.setNote("asd");
 			}
 			in.close();
 			out.close();
